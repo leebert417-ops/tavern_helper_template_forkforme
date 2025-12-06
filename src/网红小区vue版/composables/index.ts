b5f9fb942039b66e0d3f99407d5a461b87a9b2a8
leebert417-ops <leebert417@gmail.com>
2025-12-06 18:28:12 +0800

    删除“网红小区-vue版”

--- src/网红小区vue版/composables/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Composables 统一导出
- */
-
-export { useDrag, type UseDragOptions, type UseDragReturn } from './useDrag';
-export { useMVUData, type UseMVUDataReturn } from './useMVUData';
-export { usePosition, type UsePositionOptions, type UsePositionReturn } from './usePosition';
-export { useAutoRefresh, type UseAutoRefreshOptions, type UseAutoRefreshReturn } from './useAutoRefresh';
-
--- src/网红小区vue版/stores/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Stores 统一导出
- */
-
-export { useApartmentStore } from './apartment';
-export { useUIStore } from './ui';
-export { useSettingsStore } from './settings';
-
